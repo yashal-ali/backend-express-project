dede2be56785c73c8594527d508eb8c6def3e1b8
yashal-ali <yashalalifarooqui30@gmail.com>
2024-12-30 18:37:13 +0500

    register

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,43 @@
-import { asyncHandler} from "./../utils/asyncHandler.js"
+import { asyncHandler} from "./../utils/asyncHandler.js";
+import{ ApiError }from "./../utils/ApiError.js"
+import {User} from "./../models/user.model.js"
+import { ApiResponse } from "../utils/ApiResonse.js";
 
 const registerUser =asyncHandler(async (req,res) => {
-    res.status(200).jsoon({
-        message:"ok"
+    const {fullName , userName , email ,password}=req.body
+    if ([fullName,userName,email,password].some((field)=>field?.trim()==="")){
+        throw new ApiError(400,"All field are requried")
+        
+    }
+    const existingUser=User.findOne({
+        $or :[{email} , {userName}]
     })
+    if(existingUser){
+        throw new ApiError (409 , "User alreading registered")
+    }
+    const avatarLocal=req.files?.avatar[0]?.path
+    const coverImageLocal=req.files?.coverImage[0]?.path
+    if(!avatarLocal){
+        throw new ApiError (400 , "Avatar file is required")
+    }
+    const user = await User.create ({
+        fullName,
+        avatar:avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        userName:userName.toLowerCase()
+    })
+    const createedUser= await User.findById(user._id).select(
+        "-password -refreshTOken",
+    )
+    if (!createedUser){
+        throw new ApiError(500, "Internal Server Error ")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200 , createedUser , "User registered sucessfully")
+    )    
 })
 export {
     registerUser
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,18 @@
 import { registerUser } from "../controllers/user.controller.js";
 import { Router } from "express";
+import {upload} from "./../middlewares/multer.middleware.js"
 
 const userRouter=Router()
-userRouter.route('/register').post(registerUser)
+userRouter.route('/register').post( upload.fields([
+    {
+        name:"avatar",
+        maxCount:1
+    },
+    {
+        name:"coverImage",
+        maxCount:1
+    }
+]),registerUser)
 
 
 export default  userRouter
